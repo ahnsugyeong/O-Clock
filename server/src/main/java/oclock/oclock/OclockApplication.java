832b94ca83b3644e106175df5dd0cfa4d568e095
suddiyo <suddii01@sju.ac.kr>
2022-11-04 03:03:45 +0900

    Initial commit

---
+++ server/src/main/java/oclock/oclock/OclockApplication.java
@@ -0,0 +1,14 @@
+package oclock.oclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class OclockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OclockApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/oclock/oclock/OclockApplicationTests.java
@@ -0,0 +1,13 @@
+package oclock.oclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
